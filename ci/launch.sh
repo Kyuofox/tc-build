a17a0b1402656b492939d677c929f36662fdcd65
Danny Lin <danny@kdrag0n.dev>
2020-03-06 23:03:30 -0800

    ci: launch: Use an exit trap and exit on error
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- ci/launch.sh
+++ ci/launch.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-set -ufo pipefail
+set -veufo pipefail
 cd "$(dirname "$0")"
 
 tf_vars=(-var packet_token="${1:-$PACKET_TOKEN}" -var github_token="${2:-$GH_TOKEN}" -var packet_ssh_key="${3:-$PACKET_SSH_KEY}" -var github_run_id="${GITHUB_RUN_ID:-}" -var telegram_chat_id="${4:-$TG_CHAT_ID}" -var telegram_token="${5:-$TG_TOKEN}" -var git_author_name="${6:-$GIT_AUTHOR_NAME}" -var git_author_email="${7:-$GIT_AUTHOR_EMAIL}")
 
 terraform init
+
+function finish() {
+    terraform destroy -auto-approve "${tf_vars[@]}"
+}
+trap finish EXIT
+
 terraform apply -auto-approve "${tf_vars[@]}"
-terraform destroy -auto-approve "${tf_vars[@]}"
