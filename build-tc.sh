76e37b2d77824bc3222a9fe8da447fdad626af47
KyuoFoxHuyu <88403766@qq.com>
2021-06-18 12:24:55 +0800

    built-tc: inherit build script from wloot/tc-build
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

---
+++ build-tc.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# Function to show an informational message
+function msg() {
+    echo -e "\e[1;32m$@\e[0m"
+}
+
+rm -rf installTmp
+
+# Build LLVM
+msg "Building LLVM..."
+./build-llvm.py \
+	--update \
+	--build-stage1-only \
+	--install-stage1-only \
+	--projects "clang;lld;polly" \
+	--targets "ARM;AArch64;X86" \
+	--install-folder "installTmp" \
+	--clang-vendor "Kyuofox-$(date +%Y%m%d)" \
+	--additional-build-arguments "CLANG_REPOSITORY_STRING=GitHub.com/KyuoFoxHuyu"
+
+# Build binutils
+msg "Building binutils..."
+if [ $(which clang) ] && [ $(which clang++) ]; then
+	export CC=$(which ccache)" clang"
+	export CXX=$(which ccache)" clang++"
+	[ $(which llvm-strip) ] && stripBin=llvm-strip
+else
+	export CC=$(which ccache)" gcc"
+	export CXX=$(which ccache)" g++"
+	[ $(which strip) ] && stripBin=strip
+fi
+./build-binutils.py \
+	--targets arm aarch64 x86_64 \
+	--install-folder "installTmp"
+
+# Remove unused products
+msg "Removing unused products..."
+rm -f installTmp/lib/*.a installTmp/lib/*.la installTmp/lib/clang/*/lib/linux/*.a*
+
+msg "Setting library load paths for portability and"
+msg "Stripping remaining products..."
+IFS=$'\n'
+for f in $(find installTmp -type f -exec file {} \;); do
+	if [ -n "$(echo $f | grep 'ELF .* interpreter')" ]; then
+		i=$(echo $f | awk '{print $1}'); i=${i: : -1}
+		# Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
+		if [ -d $(dirname $i)/../lib/ldscripts ]; then
+			patchelf --set-rpath '$ORIGIN/../../lib:$ORIGIN/../lib' "$i"
+		else
+			if [ "$(patchelf --print-rpath $i)" != "\$ORIGIN/../../lib:\$ORIGIN/../lib" ]; then
+				patchelf --set-rpath '$ORIGIN/../lib' "$i"
+			fi
+		fi
+		# Strip remaining products
+		if [ -n "$(echo $f | grep 'not stripped')" ]; then
+			${stripBin} --strip-unneeded "$i"
+		fi
+	elif [ -n "$(echo $f | grep 'ELF .* relocatable')" ]; then
+		if [ -n "$(echo $f | grep 'not stripped')" ]; then
+			i=$(echo $f | awk '{print $1}');
+			${stripBin} --strip-unneeded "${i: : -1}"
+		fi
+	else
+		if [ -n "$(echo $f | grep 'not stripped')" ]; then
+			i=$(echo $f | awk '{print $1}');
+			${stripBin} --strip-all "${i: : -1}"
+		fi
+	fi
+done
+
+rm -rf ./install
+mv installTmp/ install/
+
+msg "build-tc HEAD: $(git rev-parse HEAD)"
+msg "llvm-project HEAD: $(git -C llvm-project/ rev-parse HEAD)"
