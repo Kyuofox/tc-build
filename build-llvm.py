8f6bb31c618fe3181607fb383deadffb3622d3f8
Nathan Chancellor <nathan@kernel.org>
2023-08-28 17:11:42 -0700

    build-llvm.py: Update GOOD_REVISION to e280e406c2e34ce29e1e71da7cd3a284ea112ce8
    This has been qualified on aarch64 and x86_64 hosts using my
    llvm-kernel-testing repository.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- build-llvm.py
+++ build-llvm.py
@@ -14,7 +14,7 @@ from tc_build.kernel import KernelBuilder, LinuxSourceManager, LLVMKernelBuilder
 from tc_build.tools import HostTools, StageTools
 
 # This is a known good revision of LLVM for building the kernel
-GOOD_REVISION = '5d79a8d148418da0565e6aa665178e6f095dd73e'
+GOOD_REVISION = 'e280e406c2e34ce29e1e71da7cd3a284ea112ce8'
 
 parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
 clone_options = parser.add_mutually_exclusive_group()
